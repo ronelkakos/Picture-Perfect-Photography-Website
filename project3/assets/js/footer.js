397c7b31458c7e6fb4ac839609715addb5108264
ronelkakos <134534386+ronelkakos@users.noreply.github.com>
2024-09-20 15:53:12 -0400

    Project Three All Files for Website

---
+++ project3/assets/js/footer.js
@@ -0,0 +1,10 @@
+<footer>
+<a href="#home">Home |</a>
+<a href="#rules">Rules |</a>
+<a href="#submit">Submit |</a>
+<a href="#category">Categories |</a>
+<a href="#vote">Voting |</a>
+<a href="#gallery">Gallery</a>
+</footer>
+
+
